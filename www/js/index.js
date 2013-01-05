7dbea72e62d424e61486e391be793d00f8f0e785
jerone <jeronevw@hotmail.com>
2013-01-06 00:38:28 +0100

    Clean up
    Renamed main to index.
    Removed jQuery support for now.
    Added onload event.

---
+++ www/js/index.js
@@ -0,0 +1,21 @@
+requirejs.config({
+	baseUrl: 'js',
+	paths: {},
+	shim: {}
+});
+
+
+window.addEventListener('load', function() {
+	require(['config', 'Quintus', 'player', 'exit', 'enemy', 'level-1', 'endGame'], function (config, Q) {
+
+		console.log("Game starting...", !!Q);
+		window.Q = Q;
+
+		Q.load("sprites.png, sprites.json, level-1.json, tiles.png", function () {
+			Q.sheet("tiles", "tiles.png", { tilew: config.spriteDimension, tileh: config.spriteDimension });
+			Q.compileSheets("sprites.png", "sprites.json");
+			Q.stageScene("level-1");
+		});
+
+	});
+});
